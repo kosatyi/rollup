a23669cb6dbb44cb8c89ff405dccfb23f7f79e36
Stepan Kosatyi <stepan@kosatyi.com>
2024-03-11 12:53:20 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
+import { promises as fs } from 'fs'
 import utils from '@rollup/pluginutils'
 import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 import babel from '@rollup/plugin-babel'
 import terser from '@rollup/plugin-terser'
+import json from '@rollup/plugin-json'
 import typescript from '@rollup/plugin-typescript'
 import ignore from 'rollup-plugin-ignore'
 import scss from 'rollup-plugin-scss'
 import copy from 'rollup-plugin-copy'
 import cleanup from 'rollup-plugin-cleanup'
 import watch from 'rollup-plugin-watch'
-import { promises as fs } from 'fs'
 
 const jsonFile = (target, content) => {
     return {
@@ -25,6 +26,7 @@ const jsonFile = (target, content) => {
 export {
     jsonFile,
     utils,
+    json,
     watch,
     cleanup,
     copy,
